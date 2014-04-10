3c64b486f6ecf7df3bbf9adc94fcc6da1e2caebb
Jesper Persson <jespert.persson@gmail.com>
2014-04-10 23:24:06 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include <SFML/Graphics.hpp>
+#include <iostream> 
+
+int main()
+{
+    sf::Texture texture;
+    if (!texture.loadFromFile("player.png", sf::IntRect(0, 16, 16, 16)))
+    {
+        exit(0);
+    }
+    texture.setSmooth(true);
+    // create the window
+    sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
+    sf::Sprite sprite;
+    sprite.setTexture(texture);
+    sprite.scale(sf::Vector2f(3.f, 3.f));
+
+    // run the program as long as the window is open
+    while (window.isOpen())
+    {
+        // check all the window's events that were triggered since the last iteration of the loop
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            // "close requested" event: we close the window
+            if (event.type == sf::Event::Closed)
+                window.close();
+        }
+
+        // clear the window with black color
+        window.clear(sf::Color::Black);
+
+        window.draw(sprite);
+
+        // end the current frame
+        window.display();
+    }
+
+    return 0;
+}
